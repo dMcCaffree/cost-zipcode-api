c50570181054c1b50990f083feef34081615d88e
Dustin McCaffree <dustin@smartystreets.com>
2017-07-28 16:43:26 -0600

    Finished the server to connect to mongoose.

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require('express'),
 	app = express(),
-	port = process.env.PORT || 3000;
+	port = process.env.PORT || 3000,
+	mongoose = require('mongoose'),
+	Project = require('./api/models/costModel'),
+	bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/projectsDb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/costRoutes');
+routes(app);
 
 app.listen(port);
 
-console.log("ZIP Code Cost API server now started on: " + port);
+console.log('ZIP Code Cost API server started on: ' + port);
